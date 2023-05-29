7ba59670a94d63a5a528828ef887335a2abd2fb5
Moises <moisalas@gmail.com>
2023-05-29 11:27:17 +0200

    Modified mlapi.py file: Updated API endpoint handle

--- mlapi.py
+++ mlapi.py
@@ -1,13 +1,21 @@
 # bring in 
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+from fastapi.middleware.cors import CORSMiddleware
 
 
 from gramformer import Gramformer
 import torch
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://www.aiwrightassistant.com/", "https://www.aiwrightassistant.com/", "http://www.aiwrightassistant.com/wright/", "https://www.aiwrightassistant.com/wright/"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class CorrectingItem(BaseModel):
     sentence: str
